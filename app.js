7328bf42c03d7e1d3226b65560c81a8f37017252
armanDark <naprimerarman@gmail.com>
2021-06-10 18:29:23 +0400

    swagger doc

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 const { development: { serverHost, serverPort } } = require('./config');
 // db
 const { sequelize } = require('./models');
@@ -9,10 +11,30 @@ const responseHandler = require('./middleware/responseHandler');
 // routes
 const { authRouter, usersRouter } = require('./routes');
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Users API',
+      version: '1.0.0',
+      description: 'A simple users API',
+    },
+    servers: [
+      {
+        url: `http://${serverHost}:${serverPort}`,
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+const swaggerSpecs = swaggerJSDoc(swaggerOptions);
+
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 
 // routes
 app.use((req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,50 @@ const userValidation = require('../utils/validation/userValidation');
 const transformJoiException = require('../utils/exceptions/transformJoiException');
 
 const router = express.Router();
+/**
+ * @swagger
+ * /auth:
+ *  post:
+ *    tags:
+ *    - auth
+ *    summary: Authenticate
+ *    description: Authenticate with username and password and get a JWT token.
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          type: object
+ *          schema:
+ *            properties:
+ *              username:
+ *                type: string
+ *                required: true
+ *              password:
+ *                type: string
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: JWT token with an expiration time of 30 minutes
+ *        content:
+ *          application/json:
+ *            type: object
+ *            schema:
+ *              properties:
+ *                accessToken:
+ *                  type: string
+ *      400:
+ *        description: Bad request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 
 router.post('/', async (req, res, next) => {
   const { username, password } = req.body;
